37c3df99056ed292153b2b7a34ec765462d9139b
Ilfat <shakirovilfat@gmail.com>
2016-06-07 12:48:09 -0700

    add ex30

---
+++ Week02/ex30.py
@@ -0,0 +1,24 @@
+peop = 30
+cars = 40
+trucks = 15
+
+if cars > peop:
+    print "We should take th car"
+elif cars < peop:
+    print "We should not take th car"
+else:
+    print "We can't decide"
+
+if trucks > peop:
+    print "Too many trucks"
+elif trucks < peop:
+    print "Maybe we could take trucks"
+else:
+    print "We still can't decide"
+    
+if trucks < peop:
+    print "Allrigth"
+elif trucks < peop:
+    print "Maybe we could take trucks"
+else:
+    print "Fine, let's stay home then"  
